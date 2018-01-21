d694e92c28655c5ccae71c944b554db51cd76fdd
matamnikhil <nikhil.kumar377@gmail.com>
2018-01-21 11:21:33 +0530

    Add project files.

---
+++ CoSwap1/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoSwap1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();//done
+        }
+
+       
+        public ActionResult ColIntro()
+        {
+            return View();//done
+        }
+
+        public ActionResult CollateralSwap()
+        {
+            return View();//done
+        }
+
+        public ActionResult CollateralSwapPlatform()
+        {
+            return View();
+        }
+
+        public ActionResult RepoIntro()
+        {
+            return View();//done
+        }
+
+        public ActionResult Repurchase()
+        {
+            return View();//done
+        }
+
+        public ActionResult RepurchasePlatform()
+        {
+            return View();//done
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();//done
+        }
+
+        public ActionResult ManagementTeam()
+        {
+            return View();//done
+        }
+
+    }
+}
